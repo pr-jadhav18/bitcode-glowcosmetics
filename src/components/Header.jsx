230dddffaad465bd231bfbd9b9ecb5f11fd31a51
pr-jadhav18 <poojaborse173@gmail.com>
2025-04-08 23:23:51 +0530

    making footer and add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Image } from 'react-bootstrap';
 
 function Header() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="w-100 fixed-top">
+    <Navbar  data-bs-theme="dark" expand="lg" className="w-100 fixed-top bg-info">
       <Container>
-        <Navbar.Brand href="/">Glow-Cosmetics</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/features">Features</Nav.Link>
-          <Nav.Link href="/pricing">Pricing</Nav.Link>
+        <Navbar.Brand href="/">
+        <Image src="/logo.png" width={80} height={80} />
+        </Navbar.Brand>
+        <Nav className="me-auto text-dark">
+          <Nav.Link href="/" className='text-dark fw-bold me-4' >Home</Nav.Link>
+          <Nav.Link href="/shop"  className='text-dark fw-bold me-4'>Shop</Nav.Link>
+          <Nav.Link href="/aboutus"  className='text-dark fw-bold me-4'>Category</Nav.Link>
+          <Nav.Link href="/aboutus"  className='text-dark fw-bold me-4'>About Us</Nav.Link>
+          <Nav.Link href="/aboutus"  className='text-dark fw-bold me-4'>Contact Us</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
