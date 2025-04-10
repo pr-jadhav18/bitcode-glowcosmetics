b048ea05a1ffedf71438ef7986d248d91ca8c706
pr-jadhav18 <poojaborse173@gmail.com>
2025-04-11 00:09:10 +0530

    making products  and categories in home page

---
+++ src/pages/ProductCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Card, Button, Col ,Container,Row} from "react-bootstrap";
+
+const ProductCard = () => {
+ const Products = [
+    { Name:"Lipstick", Image: "/Categories/lipstic.webp", Price:"100", Desc:"Long-lasting matte lipstick in bold shades." },
+    { Name:"Makeup Brush Set", Image:"/Categories/category-2.webp", Price:"200", Desc:"Premium quality brush set for flawless makeup application."}
+ ]
+
+  return (
+    <Container className='mt-3 mb-3'>
+       <p className='text-center fw-bold gradient-text fs-2 '>Feature Products</p>
+      <Row>
+        {
+          Products.map((prod,index)=> (
+            <Col xs={6} md={3} className='text-center flex-shrink:0' key={index}>
+                <Card className="shadow-sm rounded-4" style={{ width: "18rem", border: "1px solid #ddd" }}>
+                  <Card.Img
+                    variant="top"
+                    src={prod.Image}
+                    style={{
+                      height: "250px",
+                      objectFit: "cover",
+                      borderBottom: "1px solid #eee",
+                    }}
+                  />
+                  <Card.Body>
+                    <Card.Title className="fw-bold fs-5">{prod.Name}</Card.Title>
+                    <Card.Text className="text-muted" style={{ height: "60px" }}>
+                     {prod.Desc}
+                    </Card.Text>
+                    <div className="d-flex justify-content-between align-items-center mt-3">
+                      <span className="fw-bold fs-5 text-success">₹{prod.Price}</span>
+                      <Button variant="dark" >
+                        Add to Cart
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+            </Col>
+          ))
+        }
+        
+      </Row>
+    </Container>
+  );
+}
+
+export default ProductCard;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
+import Category from './pages/Category';
 
 const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/categories' element={<Category/>}></Route>
+        
     </Routes>
   );
 }
